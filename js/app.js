c248077140fae42da0b496a4fc6e8841eafcd3ed
programmer-jafir <jafiranjum2020@gmail.com>
2022-02-17 16:07:50 +0600

    Saving Amount and Remaining Balance completed

--- js/app.js
+++ js/app.js
@@ -5,38 +5,55 @@ function Item(Id){
         return ItemTextNum;
 };
 
-    // Calculate started 
-
-document.getElementById('Calculate').addEventListener('click', function(event){
-
-    const Income = Item('income');
+function itemsTotal(){
     const Food = Item('food');
     const Rent = Item('rent');
     const Clothes = Item('clothes');
     const total = Food + Rent + Clothes;
+    return total;
+}
+
+function Balance(){
+    const Balance = document.getElementById('balance');
+    const BalanceText = parseFloat(Balance.innerText);
+    Balance.innerText = Item('income') - itemsTotal();
+    return Balance.innerText;
+};
+    // Calculate started 
+
+document.getElementById('Calculate').addEventListener('click', function(event){
+
+
 
     // Total Expenses
 
     const totalAmount = document.getElementById('total-expenses');
     const totalAmountText = totalAmount.innerText;
-    totalAmount.innerText = total;
+    totalAmount.innerText = itemsTotal();
 
     // Balance update 
 
-    const Balance = document.getElementById('balance');
-    const BalanceText = Balance.innerText;
-    Balance.innerText = Income - total;
-
+    Balance();
     
 });
 function saveAmount(){
+
+    // Save Amount persentence
+
 const saveAmount = document.getElementById('save-input');
 const saveAmountInput = saveAmount.value;
 const saveAmountDevided  = Item('income') / parseFloat(100);
 const saveAmountMultiplay = saveAmountDevided * saveAmountInput;
-console.log(saveAmountMultiplay);
+
+    // Save amount output
 
 const saveAmountOutput = document.getElementById('save-amount');
     const saveAmountOutputText = saveAmountOutput.innerText;
     saveAmountOutput.innerText = saveAmountMultiplay;
-};
+
+    // Remaining Balance
+
+    const RemainingAmount = document.getElementById('remaining-balance');
+    const RemainingAmountText = RemainingAmount.innerText;
+    RemainingAmount.innerText = Balance() -  saveAmountOutput.innerText;
+}
